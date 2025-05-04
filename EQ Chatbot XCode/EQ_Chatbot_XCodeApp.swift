8e7b4283d16dc9b9b7e57e273969e5ad3720f46c
lyncht248 <lyncht248@gmail.com>
2025-05-04 11:27:36 +0100

    Initial Commit

---
+++ EQ Chatbot XCode/EQ_Chatbot_XCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EQ_Chatbot_XCodeApp.swift
+//  EQ Chatbot XCode
+//
+//  Created by Thomas Lynch on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EQ_Chatbot_XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EQ Chatbot XCodeTests/EQ_Chatbot_XCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  EQ_Chatbot_XCodeTests.swift
+//  EQ Chatbot XCodeTests
+//
+//  Created by Thomas Lynch on 04/05/2025.
+//
+
+import Testing
+@testable import EQ_Chatbot_XCode
+
+struct EQ_Chatbot_XCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
